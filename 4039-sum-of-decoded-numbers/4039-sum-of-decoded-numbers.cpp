976ab054c33783aeeae8bde1f88aa19ed70edd91
Sundhara Priyan S <karthiselvam4352@gmail.com>
2026-08-30 09:34:46 +0530

    Time: 108 ms (59.71%), Space: 91 MB (20.39%) - LeetHub

---
+++ 4039-sum-of-decoded-numbers/4039-sum-of-decoded-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int sumDecoded(vector<long long>& nums) {
+        long long c=0;
+        long long t=1e9+7;
+        for (int i=0;i<nums.size();i++){
+            int w=nums[i]%10;
+            long long f=nums[i]/10;
+            string s=to_string(f);
+            long long a=stoll(s.substr(0,w));
+            long long b=stoll(s.substr(w));
+            long long ne=1;
+            while (b>0){
+                if (b%2)
+                    ne=(ne*a)%t;
+                a=(a*a)%t;
+                b/=2;
+            }
+            c=(c+ne)%t;
+        }
+        return c%t;
+    }
+};
